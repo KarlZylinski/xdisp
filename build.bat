2956de9eb601c7db1a553fd9534f96e499bbcc66
Karl Zylinski <karl@zylinski.se>
2016-08-17 21:25:33 +0200

    copy debug data.

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-cl.exe /D _HAS_EXCEPTIONS=0 /nologo /W4 /WX /EHsc /wd4055 /wd4054 /wd4152 /wd4204 /D _CRT_SECURE_NO_WARNINGS /Zi /MTd -c /Foxdisp.o xdisp.c
-lib.exe /out:xdisp.lib kernel32.lib user32.lib opengl32.lib gdi32.lib xdisp.o
+call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+cl.exe /D _HAS_EXCEPTIONS=0 /nologo /W4 /WX /EHsc /wd4055 /wd4054 /TC /wd4152 /wd4204 /d2noftol3 /D _CRT_SECURE_NO_WARNINGS /Zi /MTd -c /Foxdisp.o xdisp.c
+lib.exe /out:xdisp.lib kernel32.lib gdi32.lib user32.lib opengl32.lib xdisp.o
 copy /Y xdisp.lib d:\projects\snake
+copy /Y vc140.pdb d:\projects\snake
 rem link.exe /DEBUG /out:xdisp.exe xdisp.o kernel32.lib user32.lib opengl32.lib gdi32.lib
